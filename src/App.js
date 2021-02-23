b3752cabee0c24ac6e9eb71770d61a5320770d08
Szymon <s.stawowy@pleodigital.com>
2021-02-23 08:27:21 +0100

    app fixed

--- src/App.js
+++ src/App.js
@@ -5,12 +5,12 @@ import './App.css';
 // Import the Today component to be used below
 import Today from './Today/Today'
 // Import the History component to be used below
-import History from './History/History'
+// import History from './History/History'
 
 class App extends Component {
 	render() {
 		return (
-			<div className="">
+			<div>
 				<div className="topheader">
 					<header className="container">
 						<nav className="navbar">
@@ -29,7 +29,7 @@ class App extends Component {
 					</div>
 					<div className="results--section__inner">
 						<Today />
-						<History />
+						{/*<History />*/}
 					</div>
 				</section>
 			</div>
